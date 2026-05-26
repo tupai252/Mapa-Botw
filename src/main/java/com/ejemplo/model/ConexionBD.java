3332493a50602bb27c30b0eafc55cb3bdec8523f
unknown <Admin-Dit@DIT-071.digitechmad.local>
2026-05-26 10:49:23 +0200

    Update DB connection for Railway

--- src/main/java/com/ejemplo/model/ConexionBD.java
+++ src/main/java/com/ejemplo/model/ConexionBD.java
@@ -6,13 +6,13 @@ import java.sql.SQLException;
 
 public class ConexionBD {
 
-    private static final String DB_HOST = System.getenv().getOrDefault("DB_HOST", "mysql");
-    private static final String DB_PORT = System.getenv().getOrDefault("DB_PORT", "3306");
-    private static final String DB_NAME = System.getenv().getOrDefault("DB_NAME", "bd1");
-    private static final String DB_USER = System.getenv().getOrDefault("DB_USER", "root");
-    private static final String DB_PASSWORD = System.getenv().getOrDefault("DB_PASSWORD", "root");
+    private static final String MYSQLHOST = System.getenv().getOrDefault("MYSQLHOST", "mysql.railway.internal");
+    private static final String MYSQLPORT = System.getenv().getOrDefault("MYSQLPORT", "3306");
+    private static final String MYSQL_DATABASE = System.getenv().getOrDefault("MYSQL_DATABASE", "railway");
+    private static final String MYSQLUSER = System.getenv().getOrDefault("MYSQLUSER", "root");
+    private static final String MYSQLPASSWORD = System.getenv().getOrDefault("MYSQLPASSWORD", "nZFWrmujVemXiMEWyzLYplhtcBtizMwt");
 
-    private static final String URL = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME
+    private static final String URL = "jdbc:mysql://" + MYSQLHOST + ":" + MYSQLPORT + "/" + MYSQL_DATABASE
             + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
     static {
@@ -24,6 +24,6 @@ public class ConexionBD {
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, DB_USER, DB_PASSWORD);
+        return DriverManager.getConnection(URL, MYSQLUSER, MYSQLPASSWORD);
     }
 }
